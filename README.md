a5bc1462698afe11c1f9b33a3b50e05ebf96af7c
Fangdun Cai (Fundon) cfddream@gmail.com <cfddream@gmail.com>
2016-07-15 23:06:05 +0800

    Rating

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rating
+
+Rating component is based on [starability](http://lunarlogic.github.io/starability/) for Vue Bulma.
+
+## Installation
+
+```
+$ npm install vue-bulma-rating
+```
+
+## Examples
+
+
+## Badges
+
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+![](https://img.shields.io/badge/status-stable-green.svg)
+
+---
+
+> [fundon.me](https://fundun.me) &nbsp;&middot;&nbsp;
+> GitHub [@fundon](https://github.com/fundon) &nbsp;&middot;&nbsp;
+> Twitter [@_fundon](https://twitter.com/_fundon)
+
---
+++ src/Rating.vue
@@ -0,0 +1,37 @@
+<template>
+  <fieldset class="starability-basic rating">
+    <legend v-if="legend">{{ legend }}</legend>
+  <template v-for="item in items">
+    <input type="radio" :id="uuid($index)" name="rating" :value="item.value" />
+    <label :for="uuid($index)" :title="item.title || ''">{{ item.label || '' }}</label>
+  </template>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  props: {
+    legend: String,
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  methods: {
+    uuid (index) {
+      return `rating-${this._uid}-item-${index}`
+    }
+  },
+
+  computed: {
+    count () {
+      return this.items.length
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~starability/starability-scss/starability/all";
+</style>
